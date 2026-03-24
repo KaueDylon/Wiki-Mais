ed2096fdbfe050dbf353d1d81a2339a258eb65ca
Kaue Dylon <kaue.dylon33@gmail.com>
2026-03-24 08:41:28 -0300

    commit parcial 2

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import dao.ArtigoDAO;
+import dao.CategoriaDAO;
+import dao.UsuarioDAO;
+import entity.Artigo;
+import entity.Categoria;
+import entity.Usuario;
+
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        //        Scanner scanner = new Scanner(System.in);    FUNCIONANDO
+//
+//        System.out.println("Teste");
+//        String nome = scanner.nextLine();
+//        System.out.println("Teste2");
+//        String email = scanner.nextLine();
+//        System.out.println("Teste3");
+//        String senha = scanner.nextLine();
+//
+//        Usuario usuario = new Usuario(1,nome,email,senha);
+//
+//        UsuarioDAO usuarioDAO = new UsuarioDAO();
+//        usuarioDAO.cadastrarUsuario(usuario);
+
+        //======================
+
+//        CategoriaDAO categoriaDAO = new CategoriaDAO();         FUNCIONANDO
+//
+//        List<Categoria> listarCategorias = categoriaDAO.listarCategorias();
+//        for(Categoria categ : listarCategorias){
+//            System.out.println("ID: "+ categ.getId()+" || Nome: "+categ.getNome());
+//        }
+
+
+        //========================
+
+//        UsuarioDAO usuarioDAO = new UsuarioDAO();
+//
+//
+//
+//        Usuario usuario = usuarioDAO.loginUsuario("Kauê","123450");
+//
+//        Artigo artigo = new Artigo("Argentina","A Argentina é um país e...");
+//
+//        ArtigoDAO artigoDAO = new ArtigoDAO();
+//
+//        artigoDAO.cadastrarArtigo(3,artigo, usuario);
+
+
+
+    }
+
+}
